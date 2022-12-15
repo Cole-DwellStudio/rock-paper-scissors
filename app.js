38c8a7044e61b787f06e53ce9e81d040e905f056
Cole Harris <cole@dwellstudioco.com>
2022-12-14 17:18:04 -0700

    Add basic UI and styling

--- app.js
+++ app.js
@@ -1,71 +1,100 @@
-function getComputerChoice() {
-  let validChoices = ["rock", "paper", "scissors"];
-  let computerChoice;
-  computerChoice = validChoices[Math.floor(Math.random() * 3)];
-  return computerChoice;
-}
+const buttons = document.querySelectorAll("button");
+const playerScoreDiv = document.querySelector(".playerScore");
+const computerScoreDiv = document.querySelector(".computerScore");
+const ScoreDiv = document.querySelector(".score");
+const computerPlay = document.querySelector(".computerPlay");
+const playerPlay = document.querySelector(".playerPlay");
 
-function getPlayerChoice() {
-  let validChoices = ["rock", "paper", "scissors"];
-  let playerChoice = prompt(
-    "Please enter rock, paper, or scissors:"
-  ).toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
 
-  if (
-    playerChoice === "rock" ||
-    playerChoice === "paper" ||
-    playerChoice === "scissors"
-  ) {
-    return playerChoice;
-  } else {
-    alert("Invalid choice!");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    playGame(button.id);
+  });
+});
+
+function playGame(playerChoice) {
+  const result = playRound(playerChoice);
+
+  // handle the result of playRound
+  if (result === "won") {
+    playerScore++;
+    ScoreDiv.textContent = `Round won`;
+  } else if (result === "lost") {
+    computerScore++;
+    ScoreDiv.textContent = `Round lost`;
+  } else if (result === "tied") {
+    // playerScore++;
+    // computerScore++;
+    ScoreDiv.textContent = `Round tied`;
+  }
+
+  // update the UI to show the current scores
+  playerScoreDiv.textContent = `Your score: ${playerScore}`;
+  computerScoreDiv.textContent = `Computer score: ${computerScore}`;
+
+  // check if the game is over, if so update UI and reset game
+  if (playerScore === 5 && computerScore === 5) {
+    ScoreDiv.textContent = "GAME TIED";
+    resetGame();
+    return;
+  } else if (playerScore >= 5) {
+    ScoreDiv.textContent = "GAME WON";
+    resetGame();
+    return;
+  } else if (computerScore >= 5) {
+    ScoreDiv.textContent = "GAME LOST";
+    resetGame();
     return;
   }
 }
 
-function playRound(playerChoice, computerChoice) {
-  if (playerChoice === computerChoice) {
-    return "tie";
-  } else if (
-    (computerChoice === "rock" && playerChoice === "scissors") ||
-    (computerChoice === "paper" && playerChoice === "rock") ||
-    (computerChoice === "scissors" && playerChoice === "paper")
-  ) {
-    return "lose";
-  } else if (
-    (computerChoice === "rock" && playerChoice === "paper") ||
-    (computerChoice === "scissors" && playerChoice === "rock") ||
-    (computerChoice === "paper" && playerChoice === "scissors")
-  ) {
-    return "win";
+function playRound(playerChoice) {
+  playerPlay.textContent = playerChoice;
+  let computerChoice = getComputerChoice();
+
+  // handle conditions and return round result
+  switch (playerChoice) {
+    case "rock":
+      if (computerChoice === "rock") {
+        return "tied";
+      } else if (computerChoice === "paper") {
+        return "lost";
+      } else if (computerChoice == "scissors") {
+        return "won";
+      }
+    case "paper":
+      if (computerChoice === "paper") {
+        return "tied";
+      } else if (computerChoice === "scissors") {
+        return "lost";
+      } else if (computerChoice == "rock") {
+        return "won";
+      }
+    case "scissors":
+      if (computerChoice === "scissors") {
+        return "tied";
+      } else if (computerChoice === "rock") {
+        return "lost";
+      } else if (computerChoice == "paper") {
+        return "won";
+      }
   }
+  return "invalid";
 }
 
-function playGame(rounds) {
-  let currentResult;
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let i = 0; i < rounds; i++) {
-    if (playerScore >= 3 || computerScore >= 3) {
-      if (playerScore >= 3 && computerScore >= 3) {
-        alert("Tie!");
-      } else if (playerScore >= 3) {
-        alert("You Win!");
-      } else if (computerScore >= 3) {
-        alert("You lose");
-      }
-      return;
-    }
+function getComputerChoice() {
+  let validChoices = ["rock", "paper", "scissors"];
+  let computerChoice;
+  computerChoice = validChoices[Math.floor(Math.random() * 3)];
+  computerPlay.textContent = computerChoice;
+  return computerChoice;
+}
 
-    currentResult = playRound(getPlayerChoice(), getComputerChoice());
-    if (currentResult === "win") {
-      playerScore++;
-    } else if (currentResult === "lose") {
-      computerScore++;
-    } else if (currentResult === "tie") {
-      playerScore++;
-      computerScore++;
-    }
-    alert(`Round ${currentResult}. Score: ${playerScore} to ${computerScore}`);
-  }
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  playerScoreDiv.textContent = `Your score:`;
+  computerScoreDiv.textContent = `Computer score:`;
 }
